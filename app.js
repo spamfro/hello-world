c4625535e8277f232a1693213902e08c39882700
George <georgevs@gmail.com>
2022-06-24 11:47:12 +0300

    Fri Jun 24 11:47:12 EEST 2022

---
+++ app.js
@@ -0,0 +1,12 @@
+(function (React, ReactDOM) {
+	'use strict';
+
+	function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+	var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+	var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
+
+	const App = () => (React__default["default"].createElement("h1", null, "Hello world!"));
+	ReactDOM__default["default"].render(React__default["default"].createElement(App, null), document.getElementById('app'));
+
+})(React, ReactDOM);
